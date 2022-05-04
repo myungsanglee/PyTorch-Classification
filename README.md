d9cac44ea3438c597dad61be5676b900ded1766f
myungsanglee <lee0577@naver.com>
2022-05-04 16:18:58 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# PyTorch-Classification
+# PyTorch Classification
+PyTorch 기반 Classification 모델 구조 및 학습 기법을 테스트하기 위한 프로젝트
+
+## Implementations
+ * Darknet19
+ * VGG16
+
+## TODOs
+- [x] ~~Darknet19~~
+- [x] ~~VGG16~~
+- [ ] Daknet53
+- [ ] ResNet
+
+## Requirements
+* `PyTorch >= 1.8.1`
+* `PyTorch Lightning`
+* `Albumentations`
+* `PyYaml`
+
+## Train Detector
+```python
+python train_classifier.py --cfg configs/darknet19_tiny-imagenet.yaml
+```
+
+## Test Detector
+```python
+python test_classifier.py --cfg configs/darknet19_tiny-imagenet.yaml
+```
---
+++ dataset/classfication/tiny_imagenet.py
@@ -0,0 +1,157 @@
+import os
+import sys  
+import glob
+sys.path.append(os.getcwd())
+
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
+import albumentations
+import albumentations.pytorch
+import cv2
+import numpy as np
+
+
+class TinyImageNetDataset(Dataset):
+    def __init__(self, dataset_dir, transforms, is_train):
+        super().__init__()
+        self.transforms = transforms
+        self.is_train = is_train
+        with open(os.path.join(dataset_dir, 'wnids.txt'), 'r') as f:
+            self.label_list = f.read().splitlines()
+
+        if is_train:
+            self.data = glob.glob(os.path.join(dataset_dir, 'train/*/images/*.JPEG'))
+            self.train_list = dict()
+            for data in self.data:
+                label = data.split(os.sep)[-3]
+                self.train_list[data] = self.label_list.index(label)
+        else:
+            self.data = glob.glob(os.path.join(dataset_dir, 'val/images/*.JPEG'))
+            self.val_list = dict()
+            with open(os.path.join(dataset_dir, 'val/val_annotations.txt'), 'r') as f:
+                val_labels = f.read().splitlines()
+                for label in val_labels:
+                    f_name, label, _, _, _, _ = label.split('\t')
+                    self.val_list[f_name] = self.label_list.index(label)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        img_file = self.data[index]
+        img = cv2.imread(img_file)
+        if self.is_train:
+            label = self.train_list[img_file]
+        else:
+            label = self.val_list[os.path.basename(img_file)]
+
+        transformed = self.transforms(image=img)['image']
+        return transformed, label
+
+
+class TinyImageNet(pl.LightningDataModule):
+    def __init__(self, dataset_dir, workers, batch_size, input_size):
+        super().__init__()
+        self.dataset_dir = dataset_dir
+        self.workers = workers
+        self.batch_size = batch_size
+        self.input_size = input_size
+        
+    def setup(self, stage=None):
+        train_transforms = albumentations.Compose([
+            albumentations.HorizontalFlip(),
+            albumentations.Blur(),
+            albumentations.ShiftScaleRotate(),
+            albumentations.GaussNoise(),
+            albumentations.Cutout(),
+            albumentations.ElasticTransform(),
+            albumentations.RandomResizedCrop(self.input_size, self.input_size, (0.8, 1)),
+            albumentations.Normalize(0, 1),
+            albumentations.pytorch.ToTensorV2(),
+        ],)
+
+        valid_transform = albumentations.Compose([
+            albumentations.Resize(self.input_size, self.input_size, always_apply=True),
+            albumentations.Normalize(0, 1),
+            albumentations.pytorch.ToTensorV2(),
+        ],)
+
+        self.train_dataset = TinyImageNetDataset(
+            dataset_dir=self.dataset_dir,
+            transforms=train_transforms,
+            is_train=True
+        )
+
+        self.valid_dataset = TinyImageNetDataset(
+            dataset_dir=self.dataset_dir,
+            transforms=valid_transform,
+            is_train=False
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.workers,
+            persistent_workers=self.workers > 0,
+            pin_memory=self.workers > 0
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.valid_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.workers,
+            persistent_workers=self.workers > 0,
+            pin_memory=self.workers > 0
+        )
+
+
+if __name__ == '__main__':
+    # test_transform = albumentations.Compose([
+    #     albumentations.HorizontalFlip(),
+    #     albumentations.Blur(),
+    #     albumentations.ShiftScaleRotate(),
+    #     albumentations.GaussNoise(),
+    #     albumentations.Cutout(),
+    #     albumentations.PiecewiseAffine(),
+    #     albumentations.Normalize(0, 1),
+    #     albumentations.pytorch.ToTensorV2(),
+    # ],)
+
+    test_transform = albumentations.Compose([
+        albumentations.Normalize(0, 1),
+        albumentations.pytorch.ToTensorV2(),
+    ],)
+
+    loader = DataLoader(
+        TinyImageNetDataset(
+            dataset_dir='/home/fssv2/myungsang/datasets/tiny_imagenet/tiny-imagenet-200',
+            transforms=test_transform,
+            is_train=False
+        ),
+        batch_size=1,
+        shuffle=True
+    )
+
+    label_name_path = '/home/fssv2/myungsang/datasets/tiny_imagenet/tiny-imagenet-200/tiny-imagenet.names'
+    with open(label_name_path, 'r') as f:
+        label_name_list = f.read().splitlines()
+
+    for sample in loader:
+        batch_x, batch_y = sample
+
+        img = batch_x[0].numpy()   
+        img = (np.transpose(img, (1, 2, 0))*255.).astype(np.uint8).copy()
+
+        print(batch_x.size())
+        print(f'label: {label_name_list[batch_y[0]]}')
+
+        cv2.imshow('sample', img)
+        key = cv2.waitKey(0)
+        if key == 27:
+            break
+
+    cv2.destroyAllWindows()
